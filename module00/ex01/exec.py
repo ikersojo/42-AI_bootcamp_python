06f2c6a9ef7f8b03d9acdb2c8b695d1fdc21409a
Iker Sojo <ikersojo@hotmail.com>
2023-01-29 09:19:20 +0100

    update

--- module00/ex01/exec.py
+++ module00/ex01/exec.py
@@ -6,7 +6,7 @@
 #    By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/28 17:47:14 by isojo-go          #+#    #+#              #
-#    Updated: 2023/01/28 22:58:04 by isojo-go         ###   ########.fr        #
+#    Updated: 2023/01/29 09:19:14 by isojo-go         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,6 +20,9 @@ if (len(sys.argv) >= 2):
 		string += sys.argv[i]
 		i += 1
 
+	# a built-in function in python to reverse strings:
+	# 	rev = string[::-1]
+	# Like in c (42 style):
 	rev = ''
 	i = len(string) - 1
 	while (i >= 0):
