6db296054c39154a97f2283432b6b83dba38a1ca
Iker Sojo <ikersojo@hotmail.com>
2023-02-04 12:44:15 +0100

    update

--- module00/ex06/recipe.py
+++ module00/ex06/recipe.py
@@ -6,7 +6,7 @@
 #    By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/04 09:15:13 by isojo-go          #+#    #+#              #
-#    Updated: 2023/02/04 11:11:29 by isojo-go         ###   ########.fr        #
+#    Updated: 2023/02/04 12:43:57 by isojo-go         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -37,23 +37,64 @@ cookbook = {
 }
 
 def add(cookbook):
+	ingr_list = []
+	meal_type = ''
+	time = 0
 	name = input(">>> Enter a name:\n")
-	ingr = input(">>> Enter ingredients:\n")
+	if (name in cookbook):
+		print("The recipe already exists.")
+	else:
+		ingr = input(">>> Enter ingredients:\n")
+		while (ingr != ''):
+			ingr_list.append(ingr)
+			ingr = input()
+		meal_type = input(">>> Enter a meal type:\n")
+		time = input(">>> Enter preparation time:\n")
+		recipe = {
+			"ingredients": ingr_list,
+			"meal": meal_type,
+			"prep_time": time
+			}
+		cookbook[name] = recipe
+
+def delete(cookbook):
+	name = input(">>> Enter a recipe to be deleted:\n")
+	try:
+		cookbook.pop(name)
+	except:
+		print("The recipe does not exist.")
 
 def print_recipe(name):
-	print(f"Recipe for {name}:")
-	i = cookbook[name]["ingredients"]
-	m = cookbook[name]["meal"]
-	p = cookbook[name]["prep_time"]
-	print(f"    Ingredients list: {i}")
-	print(f"    To be eaten for {m}.")
-	print(f"    Takes {p} minutes of cooking.")
+	if (name in cookbook):
+		print(f"Recipe for {name}:")
+		i = cookbook[name]["ingredients"]
+		m = cookbook[name]["meal"]
+		p = cookbook[name]["prep_time"]
+		print(f"    Ingredients list: {i}")
+		print(f"    To be eaten for {m}.")
+		print(f"    Takes {p} minutes of cooking.")
+	else:
+		print("The recipe does not exist.")
 
 def print_all(cookbook):
 	for key, value in cookbook.items():
 		print_recipe(key)
 		print("\n------------------\n")
 
+def print_selection():
+	print("List of available option:")
+	print("1: Add a recipe")
+	print("2: Delete a recipe")
+	print("3: Print a recipe")
+	print("4: Print the cookbook")
+	print("5: Quit\n")
+	print("Please select an option:")
+	return (int(input(">> ")))
+
 if (__name__ == "__main__"):
-	# print_recipe(sys.argv[1])
-	print_all(cookbook)
+	opt = 0
+	opt_list = (1, 2, 3, 4, 5)
+	print("Welcome to the Python Cookbook !")
+	while (opt != 5):
+		opt = print_selection
+		if (opt in  opt_list):
