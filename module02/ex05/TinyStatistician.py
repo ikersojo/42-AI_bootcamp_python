cd8494c64c16364b1b8f6d338ebc622152472315
Iker Sojo <ikersojo@hotmail.com>
2023-03-02 20:01:05 +0100

    update

--- module02/ex05/TinyStatistician.py
+++ module02/ex05/TinyStatistician.py
@@ -6,7 +6,7 @@
 #    By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/02 19:05:54 by isojo-go          #+#    #+#              #
-#    Updated: 2023/03/02 19:48:09 by isojo-go         ###   ########.fr        #
+#    Updated: 2023/03/02 20:00:17 by isojo-go         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -30,6 +30,20 @@ class TinyStatistician:
 			index = n // 2
 			return float(lst[index])
 
+	def quartile(self, lst):
+		lst = sorted(lst)
+		q1_index = int(len(lst) * 0.25)
+		q3_index = int(len(lst) * 0.75)
+		if len(lst) % 4 != 0:
+			q1 = lst[q1_index]
+			q3 = lst[q3_index]
+		else:
+			q1 = (lst[q1_index] + lst[q1_index + 1]) / 2
+			q3 = (lst[q3_index] + lst[q3_index + 1]) / 2
+		res =[float(q1), float(q3)]
+		return res
+
+
 	
 if __name__ == '__main__':
 	tstat = TinyStatistician()
@@ -39,8 +53,8 @@ if __name__ == '__main__':
 	print("      --> Expected result: 82.4")
 	print(tstat.median(a))
 	print("      --> Expected result: 42.0")
-	# print(tstat.quartile(a))
-	# print("      --> Expected result: [10.0, 59.0]")
+	print(tstat.quartile(a))
+	print("      --> Expected result: [10.0, 59.0]")
 	# print(tstat.var(a))
 	# print("      --> Expected result: 12279.439999999999")
 	# print(tstat.std(a))
