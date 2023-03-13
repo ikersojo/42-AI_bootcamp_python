c4e30ee87463a934a41a88ddf2b36d986921fc8f
Iker Sojo <ikersojo@hotmail.com>
2023-03-13 10:01:43 +0100

    update

--- module03/ex00/NumPyCreator.py
+++ module03/ex00/NumPyCreator.py
@@ -3,32 +3,55 @@
 #                                                         :::      ::::::::    #
 #    NumPyCreator.py                                    :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: isojo-go <isojo-go@student.42.fr>          +#+  +:+       +#+         #
+#    By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/02 21:47:34 by codespace         #+#    #+#              #
-#    Updated: 2023/03/03 12:59:01 by isojo-go         ###   ########.fr        #
+#    Updated: 2023/03/04 12:20:06 by isojo-go         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import numpy as np
 
-class NumPyCreator():
+class NumpyCreator():
+	def try_func(func):
+		def decorator(*args, **kwargs):
+			try:
+				return func(*args, **kwargs)
+			except:
+				return None
+		return decorator
+
+	@try_func
 	def from_list(self, lst):
-		return np.array(lst)
+		if type(lst) is not list:
+			return None
+		return np.array(lst).__repr__() #not identical ] vs. ]]
 
-	def from_tuple(self, lst):
+	@try_func
+	def from_tuple(self, tpl):
+		if type(tpl) is not tuple:
+			return None
+		return np.array(tpl).__repr__() #not identical with or without .__repr__()
+
+	@try_func
+	def from_iterable(self, itr):
+		if not hasattr(itr, '__iter__'):
+			return None
+		return np.array(itr).__repr__()
+
+	@try_func
+	def from_shape(self, shape, value=None):
+		if value == None:
+			return np.zeros(shape)
+		else:
+			return np.array(value).reshape(shape).__repr__()
+
+	@try_func
+	def random(self, shape):
 		pass
 
-	def from_iterable(self, lst):
-		pass
-
-	def from_shape(self, lst):
-		pass
-
-	def random(self, n):
-		pass
-
-	def identity(self, shape):
+	@try_func
+	def identity(self, n):
 		pass
 
 if __name__ == '__main__':
@@ -39,32 +62,33 @@ if __name__ == '__main__':
 	print("                                  [6, 3, 4]])")
 	
 
-	# print(npc.from_list([[1,2,3],[6,4]]))
-	# print("      --> Expected result: None")
+	print(npc.from_list([[1,2,3],[6,4]]))
+	print("      --> Expected result: None")
 
 
-	# print(npc.from_list([[1,2,3],['a','b','c'],[6,4,7]]))
-	# print("      --> Expected result: array([['1','2','3'],")
-	# print("                                  ['a','b','c'],")
-	# print("                                  ['6','4','7'], dtype='<U21'])")
+	print(npc.from_list([[1,2,3],['a','b','c'],[6,4,7]]))
+	print("      --> Expected result: array([['1','2','3'],")
+	print("                                  ['a','b','c'],")
+	print("                                  ['6','4','7'], dtype='<U21'])")
 
-	# print(npc.from_list(((1,2),(3,4))))
-	# print("      --> Expected result: None")
+
+	print(npc.from_list(((1,2),(3,4))))
+	print("      --> Expected result: None")
 
 	
-	# print(npc.from_tuple(["a", "b", "c"]))
-	# print("      --> Expected result: array(['a', 'b', 'c'])")
+	print(npc.from_tuple(("a", "b", "c")))
+	print("      --> Expected result: array(['a', 'b', 'c'])")
 
 
-	# print(npc.from_iterable(range(5)))
-	# print("      --> Expected result: array([0, 1, 2, 3, 4])")
+	print(npc.from_iterable(range(5)))
+	print("      --> Expected result: array([0, 1, 2, 3, 4])")
 
 
-	# shape=(3,5)
-	# print(npc.from_shape(shape))
-	# print("      --> Expected result: array([[0, 0, 0, 0, 0],")
-	# print("                                  [0, 0, 0, 0, 0],")
-	# print("                                  [0, 0, 0, 0, 0]])")
+	shape=(3,5)
+	print(npc.from_shape(shape))
+	print("      --> Expected result: array([[0, 0, 0, 0, 0],")
+	print("                                  [0, 0, 0, 0, 0],")
+	print("                                  [0, 0, 0, 0, 0]])")
 	
 
 	# print(npc.random(shape))
